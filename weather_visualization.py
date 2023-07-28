83aa5a332021323db31f6a3d2f7288607312c6ed
Keegan Hugh Kelly <kgnhughkelly@gmail.com>
2023-07-28 18:10:23 +0200

    Project Assessment | Multipage Website & Fonts

--- weather_visualization.py
+++ weather_visualization.py
@@ -1,8 +1,9 @@
 import requests
 import datetime
 import folium
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import os
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -15,12 +16,10 @@ UNITS = 'metric'  # Change to 'imperial' for Fahrenheit
 def index():
     try:
         weather_data = get_weather_data()
-        map_html = display_temperature_on_map(weather_data)
         weather_forecast = get_weather_forecast()
 
         return render_template(
             'index.html',
-            map_html=map_html,
             weather_forecast=weather_forecast
         )
     except Exception as e:
@@ -37,30 +36,31 @@ def get_weather_data():
 
     return data
 
-def display_temperature_on_map(weather_data):
-    lat = weather_data['coord']['lat']
-    lon = weather_data['coord']['lon']
-    temperature = weather_data['main']['temp']
-    CITY = weather_data['name']
+# Route for the Weather Map page
+@app.route('/weather-map')
+def weather_map():
+    try:
+        weather_data = get_weather_data()
+        return render_template('weather_map.html', weather_data=weather_data)
+    except Exception as e:
+        error_message = f"Error: {e}"
+        return render_template('error.html', error_message=error_message)
 
-    # Create the base map with the 'Default' tiles (OpenStreetMap)
-    map_city = folium.Map(location=[lat, lon], zoom_start=10, tiles='OpenStreetMap')
+# Route for the Weather Graphs page
+@app.route('/weather-graphs')
+def weather_graphs():
+    # Add code to generate weather graphs (you can use libraries like matplotlib)
 
-    # Add a tooltip to show temperature by the mouse cursor
-    tooltip = f'Temperature: {temperature}°C'
-    folium.Marker([lat, lon], popup=f'{CITY}\nTemperature: {temperature}°C', tooltip=tooltip).add_to(map_city)
+    # For now, just render the template without any data
+    return render_template('weather_graphs.html')
 
-    # Add 'Satellite' tiles using the default tile source
-    folium.TileLayer(tiles='Stamen Terrain', name='Terrain').add_to(map_city)
+# Route for the Weather Tables page
+@app.route('/weather-tables')
+def weather_tables():
+    # Add code to generate weather tables
 
-    # Create a LayerControl for map type with 'Default' (OpenStreetMap) and 'Terrain' (Stamen Terrain) options
-    map_layer_control = folium.LayerControl(position='topright', collapsed=False)
-    map_city.add_child(map_layer_control)
-
-    # Save the map as an HTML string
-    map_html = map_city.get_root().render()
-
-    return map_html
+    # For now, just render the template without any data
+    return render_template('weather_tables.html')
 
 def get_weather_forecast():
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={CITY}&units={UNITS}&appid={API_KEY}'
@@ -70,49 +70,69 @@ def get_weather_forecast():
     if 'list' not in data:
         raise Exception(f"Unable to retrieve weather forecast data. Response: {data}")
 
-    # Filter the data to get weather details for the next two weeks
-    today = datetime.date.today()
-    two_weeks_later = today + datetime.timedelta(days=14)
-    filtered_data = [item for item in data['list'] if today <= datetime.datetime.fromtimestamp(item['dt']).date() <= two_weeks_later]
-
-    # Create a list to store forecast data with cloud coverage graphics
     weather_forecast = []
 
-    for item in filtered_data:
+    for item in data['list']:
         date_time = datetime.datetime.fromtimestamp(item['dt'])
         date = date_time.date()
-        time = date_time.time()
-        temperature = item['main']['temp']
-        description = item['weather'][0]['description']
 
-        # Get the cloud coverage percentage and corresponding graphic URL
-        cloud_coverage = item['clouds']['all']
-        graphic_url = get_cloud_coverage_graphic(cloud_coverage)
+        for forecast in weather_forecast:
+            if forecast['date'] == date:
+                temperature = item['main']['temp']
+                description = item['weather'][0]['description']
 
-        weather_forecast.append({
-            'date': date,
-            'time': time,
-            'temperature': temperature,
-            'description': description,
-            'cloud_coverage': cloud_coverage,
-            'graphic_url': graphic_url
-        })
+                forecast['temperature_max'] = max(forecast['temperature_max'], temperature)
+                forecast['temperature_min'] = min(forecast['temperature_min'], temperature)
+                forecast['description'] = description
+                break
+        else:
+            day_name = date.strftime('%A')
+            temperature = item['main']['temp']
+            description = item['weather'][0]['description']
+            cloud_coverage = item['clouds']['all']
+            graphic_url = get_cloud_coverage_graphic(description)
+
+            weather_forecast.append({
+                'date': date,
+                'day_name': day_name,
+                'temperature_max': temperature,
+                'temperature_min': temperature,
+                'description': description,
+                'cloud_coverage': cloud_coverage,
+                'graphic_url': graphic_url
+            })
 
     return weather_forecast
 
-def get_cloud_coverage_graphic(cloud_coverage):
-    if cloud_coverage < 30:
+
+def get_cloud_coverage_graphic(description):
+    description_lower = description.lower()
+    
+    if 'clear' in description_lower or 'sun' in description_lower:
         return os.path.join('/static/weatherimages', 'clear.png')
-    elif 30 <= cloud_coverage < 70:
-        return os.path.join('/static/weatherimages', 'partly-cloudy.png')
-    elif 70 <= cloud_coverage < 90:
+    elif 'cloud' in description_lower:
         return os.path.join('/static/weatherimages', 'cloudy.png')
-    elif 90 <= cloud_coverage < 95:
+    
+    elif 'overcast' in description_lower:
+        return os.path.join('/static/weatherimages', 'partly-cloudy.png')
+    
+    elif 'rain' in description_lower or 'shower' in description_lower:
         return os.path.join('/static/weatherimages', 'lightrain.png')
-    elif 95 <= cloud_coverage < 100:
+    
+    elif 'rain' in description_lower or 'shower' in description_lower:
         return os.path.join('/static/weatherimages', 'heavyrain.png')
-    else:
+    
+    elif 'thunderstorm' in description_lower:
         return os.path.join('/static/weatherimages', 'thunderstorm.png')
+    
+    elif 'snow' in description_lower or 'flurry' in description_lower:
+        return os.path.join('/static/weatherimages', 'snow.png')
+    
+    elif 'mist' in description_lower or 'fog' in description_lower:
+        return os.path.join('/static/weatherimages', 'mist.png')
+    
+    else:
+        return os.path.join('/static/weatherimages', 'default.png')
 
 if __name__ == '__main__':
     app.run(debug=True)
